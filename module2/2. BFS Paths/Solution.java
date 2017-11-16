8f6c7fe2e83fa233b1e47c12bfc38d49e39022f5
ariji1 <31199415+ariji1@users.noreply.github.com>
2017-11-16 14:52:08 +0530

    Add files via upload

---
+++ module2/2. BFS Paths/Solution.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+public class Solution
+{
+public static void main(String[] args)
+	{
+    PrintWriter output = new PrintWriter(new OutputStreamWriter(System.out),true);  
+		int v = StdIn.readInt();
+		int e = StdIn.readInt();
+    int t = StdIn.readInt();
+		Graph g = new Graph(v);
+		int flag = 0;
+		for ( int i = 0;i<e;i++)
+        {
+            while (!StdIn.isEmpty()) 
+            {
+               if (flag == 0)
+               {
+                String te = StdIn.readLine();
+                flag++;
+               }
+               else
+                {
+                    String b = StdIn.readLine();
+                    String[] a = b.split(" ");
+                    int n = Integer.parseInt(a[0]);
+                    int m = Integer.parseInt(a[1]);
+                    g.addEdge(n,m);
+                }
+            }
+        }    
+          BreadthFirstPaths bfp=new BreadthFirstPaths(g,t);
+        
+          // PrintWriter output=new PrintWriter(new OutputStreamWriter(System.out),true);
+          
+          for(int i=0;i<g.V();i++)
+          {
+            Stack<Integer> stack = new Stack<Integer>();
+            if(bfp.hasPathTo(i))
+            {
+              output.printf("%d to %d (%d): "+"",t,i,bfp.distTo(i));
+              for(int j:bfp.pathTo(i))
+                // output.print(j+"-");
+
+                   stack.push(j);
+              String ll = stack.toString();   
+              output.println(ll.substring(0, ll.length() - 1));
+              // output.println();
+            }
+            else
+            {
+              output.printf("%d to %d (-): "+"",t,i);
+              output.println("not connected");
+            }
+          }
+   	}
+}
